685ac78f21bafae06dcc94cd03dfa50616e06bea
HeroesOfBalkan <markomacura003@gmail.com>
2024-10-26 09:16:56 +0200

    Putting all configs I could think of & some apps listed in .txt files

---
+++ hypr/gamemode.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+if [ "$HYPRGAMEMODE" = 1 ] ; then
+    hyprctl notify 1 5000 "rgb(8dc4de)" "Game mode is enabled"
+    hyprctl --batch "\
+        keyword animations:enabled 0;\
+        keyword decoration:drop_shadow 0;\
+        keyword decoration:blur:enabled 0;\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
+        keyword general:border_size 1;\
+        keyword decoration:rounding 0"
+    # pkill -f xava-x86_64.AppImage
+    exit
+fi
+hyprctl notify 1 5000 "rgb(8dc4de)" "Game mode is disabled"
+hyprctl reload
